1c2a684c10a927f48abdef194785d73d7a6c186f
IvoNet <webmaster@ivonet.nl>
2014-05-19 19:57:49 +0200

    Moved the update method to Items and called it. Ask yourself who is responsible for the action perfomed...
    In this case the answer is Item.

--- src/main/java/org/joyofcoding/objectcalisthenics/Item.java
+++ src/main/java/org/joyofcoding/objectcalisthenics/Item.java
@@ -11,23 +11,74 @@ public class Item {
 		this.sellIn = sellIn;
 	}
 
-	public void setSellIn(int sellIn) {
-		this.sellIn = sellIn;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setQuality(int quality) {
-		this.quality = quality;
-	}
+    public int getSellIn() {
+        return sellIn;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
 
-	public int getSellIn() {
-		return sellIn;
-	}
+    public int getQuality() {
+        return quality;
+    }
 
-	public int getQuality() {
-		return quality;
-	}
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public void update() {
+        if (!getName().equals("Aged Brie") && !getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (getQuality() > 0) {
+                if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+                    setQuality(getQuality() - 1);
+                }
+            }
+        } else {
+            if (getQuality() < 50) {
+                setQuality(getQuality() + 1);
+
+                if (getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (getSellIn() < 11) {
+                        if (getQuality() < 50) {
+                            setQuality(getQuality() + 1);
+                        }
+                    }
+
+                    if (getSellIn() < 6) {
+                        if (getQuality() < 50) {
+                            setQuality(getQuality() + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+            setSellIn(getSellIn() - 1);
+        }
+
+        if (getSellIn() < 0) {
+            if (!getName().equals("Aged Brie")) {
+                if (!getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (getQuality() > 0) {
+                        if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+                            setQuality(getQuality() - 1);
+                        }
+                    }
+                } else {
+                    setQuality(getQuality() - getQuality());
+                }
+            } else {
+                if (getQuality() < 50) {
+                    setQuality(getQuality() + 1);
+                }
+            }
+        }
+
+    }
 }
