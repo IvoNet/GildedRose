626f676cae5e16f00973a3db8b9d6908bb9b084d
IvoNet <webmaster@ivonet.nl>
2014-05-19 23:18:10 +0200

    Don't forget to remove all the AgedBrie stuff from the origional update method in Item...

--- src/main/java/org/joyofcoding/objectcalisthenics/model/Item.java
+++ src/main/java/org/joyofcoding/objectcalisthenics/model/Item.java
@@ -1,15 +1,15 @@
 package org.joyofcoding.objectcalisthenics.model;
 
 public class Item {
-	private String name;
-	private int sellIn;
-	private int quality;
+    private String name;
+    private int sellIn;
+    private int quality;
 
-	public Item(String name, int sellIn, int quality) {
-		this.name = name;
-		this.quality = quality;
-		this.sellIn = sellIn;
-	}
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.quality = quality;
+        this.sellIn = sellIn;
+    }
 
     public String getName() {
         return name;
@@ -32,7 +32,8 @@ public class Item {
     }
 
     public void update() {
-        if (!getName().equals("Aged Brie") && !getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+
             if (getQuality() > 0) {
                 if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
                     setQuality(getQuality() - 1);
@@ -63,22 +64,16 @@ public class Item {
         }
 
         if (getSellIn() < 0) {
-            if (!getName().equals("Aged Brie")) {
-                if (!getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (getQuality() > 0) {
-                        if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
-                            setQuality(getQuality() - 1);
-                        }
+            if (!getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (getQuality() > 0) {
+                    if (!getName().equals("Sulfuras, Hand of Ragnaros")) {
+                        setQuality(getQuality() - 1);
                     }
-                } else {
-                    setQuality(getQuality() - getQuality());
                 }
             } else {
-                if (getQuality() < 50) {
-                    setQuality(getQuality() + 1);
-                }
+                setQuality(getQuality() - getQuality());
             }
         }
-
     }
+
 }
